e66206c8dfe5fef7ee772779ed3a8b0f3699bf3d
shai2268 <askari_shahbaz@hotmail.com>
2019-09-04 17:52:54 -0500

    ability to change port number

--- src/main/java/com/fairwarning/api/FairwarningApi.java
+++ src/main/java/com/fairwarning/api/FairwarningApi.java
@@ -1,5 +1,7 @@
 package com.fairwarning.api;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,7 +15,12 @@ public class FairwarningApi {
 		// annotations explicitly
 		// *note* : we will be running our program from here as java application. Since
 		// SpringBoot has already embedded TomCat Server.
-		SpringApplication.run(FairwarningApi.class, args);
+		/* SpringApplication.run(FairwarningApi.class, args); */
+		
+		 SpringApplication app = new SpringApplication(FairwarningApi.class);
+	        app.setDefaultProperties(Collections
+	          .singletonMap("server.port", "8080"));    // if you want to change the port number, you can change it here.
+	        app.run(args);
 	}
 
 }
